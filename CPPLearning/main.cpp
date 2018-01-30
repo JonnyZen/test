0af90e9d86653b413d874fd60eabc129bd8e6b5d
JonnyZen <Jonny_Zen@live.com>
2018-01-30 17:01:57 +0800

    Initial Commit

---
+++ CPPLearning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPPLearning
+//
+//  Created by 时雨 on 2018/1/30.
+//  Copyright © 2018年 Jonny_Zen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
